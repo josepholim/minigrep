8c74567175cd72d35dde079da4536560bc2d0190
Joseph Oliver <joseph.o.lim@gmail.com>
2025-07-01 02:27:15 +0800

    search to use iterators

--- src/lib.rs
+++ src/lib.rs
@@ -23,7 +23,7 @@ impl Config {
             Some(arg) => arg,
             None => return Err("Didn't get a file path"),
         };
-        
+
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config {
@@ -51,15 +51,10 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(
@@ -67,15 +62,11 @@ pub fn search_case_insensitive<'a>(
     contents: &'a str
 ) -> Vec<&'a str> {
     let query = query.to_lowercase();
-    let mut results = Vec::new();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
     
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
